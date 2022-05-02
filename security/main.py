c258ca88d87257c996184ae2157420f0e75afec4
azzireluto <bruno.a@outlook.com.br>
2022-05-02 17:23:24 -0300

    adding security section in study files

---
+++ security/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, Depends
+from fastapi.security import OAuth2PasswordBearer
+
+app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+@app.get("/items")
+async def read_items(token: str = Depends(oauth2_scheme)):
+    return {"Token": token}
+
+# https://fastapi.tiangolo.com/pt/tutorial/security/get-current-user/
